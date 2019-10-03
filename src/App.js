d4c52eee109203efb45afb77ee1e8eee9f0ef0d4
iapolly <polina7alikina@gmail.com>
2019-10-03 22:37:54 +0300

    Add todos checking

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import styles from "./App.module.css";
 import { Button, Checkbox, Col, Input, Row } from "antd";
 
 function App() {
-  const [todos, setTodos] = useState([1, 2, 3]);
-  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([{id: 1, value: 'lala', isResolved: false}]);
+  const [input, setInput] = useState('');
 
   return (
     <div>
@@ -20,21 +20,44 @@ function App() {
       </Row>
       <Row type="flex" justify="center" align="center">
         <Col span={8}>
-          {todos.map(() => <ToDoItem/>)}
+          {todos.map((todo, key) => <ToDoItem key={key} id={todo.id} value={todo.value} isResolved={todo.isResolved} toggle={toggle}/>)}
         </Col>
       </Row>
     </div>
   );
+
+  function toggle(id) {
+    const index = todos.findIndex(todo => todo.id === id);
+
+    let todo = todos[index];
+    todo = { ...todo, isResolved: !todo.isResolved };
+
+    const newTodos = [...todos];
+    newTodos[index] = todo;
+
+    setTodos(newTodos);
+  }
+
   function addTodo() {
-    setTodos([...todos, input]);
+    const newTodo = {
+      value: input,
+      isResolved: false,
+      id: todos.length ? todos[todos.length - 1].id + 1 : 0
+    };
+    setTodos([...todos, newTodo]);
+    setInput('');
   }
 }
 
-function ToDoItem() {
+function ToDoItem(props) {
+  const { id, value, toggle, isResolved } = props;
+
   return (
-    <div className={styles.listItem}>
-      <Checkbox className={styles.checkbox}/>
-      <Button className={styles.closeButton} type="primary" shape="circle" icon="close" size="small"/>
+    <div className={styles.listItem} onClick={() => toggle(id)} >
+      <Checkbox className={styles.checkbox} checked={isResolved} />
+      <span>{value}</span>
+      <Button className={styles.closeButton} type="primary" shape="circle" icon="close" size="small" />
+      <hr/>
     </div>
   );
 }
