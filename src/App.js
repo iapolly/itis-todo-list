069130f74a351a5b144bee35155db6cdb0086205
iapolly <polina7alikina@gmail.com>
2019-10-03 18:09:44 +0300

    Add "add todo"

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import styles from  './App.module.css';
-import {Button, Col, Input, Row} from "antd";
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import { Button, Checkbox, Col, Input, Row } from "antd";
 
 function App() {
-    const todos = [1, 2, 3];
-  return (
-      <div>
-          <Row type="flex" justify="center" align="center">
-              <Col span={8}>
-                  <div className={styles.flex}>
-                      <Input />
-                      <Button type="primary" className={styles.actionButton}>:)</Button>
-                  </div>
-              </Col>
-          </Row>
-          <Row type="flex" justify="center" align="center">
+  const [todos, setTodos] = useState([1, 2, 3]);
+  const [input, setInput] = useState("");
 
-              <Col span={8}>
-                  {todos.map(() => <ToDoItem />)}
-              </Col>
-          </Row>
-      </div>
+  return (
+    <div>
+      <Row type="flex" justify="center" align="center">
+        <Col span={8}>
+          <div className={styles.flex}>
+            <Input value={input} onChange={(event) => {
+              setInput(event.target.value);
+            }}/>
+            <Button type="primary" onClick={addTodo} className={styles.actionButton}>:)</Button>
+          </div>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center" align="center">
+        <Col span={8}>
+          {todos.map(() => <ToDoItem/>)}
+        </Col>
+      </Row>
+    </div>
   );
+  function addTodo() {
+    setTodos([...todos, input]);
+  }
 }
 
 function ToDoItem() {
-    return (
-        <div className={styles.listItem}>
-            <Button className={styles.closeButton} type="primary" shape="circle" icon="close" size="small" />
-        </div>
-    )
+  return (
+    <div className={styles.listItem}>
+      <Checkbox className={styles.checkbox}/>
+      <Button className={styles.closeButton} type="primary" shape="circle" icon="close" size="small"/>
+    </div>
+  );
 }
+
 export default App;
