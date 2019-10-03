f885bb5836f830d305da2a7077245bb41b4032a3
iapolly <polina7alikina@gmail.com>
2019-10-03 17:28:11 +0300

    First lesson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import styles from  './App.module.css';
+import {Button, Col, Input, Row} from "antd";
 
 function App() {
+    const todos = [1, 2, 3];
   return (
-    <div>
-      WOOOORK
-    </div>
+      <div>
+          <Row type="flex" justify="center" align="center">
+              <Col span={8}>
+                  <div className={styles.flex}>
+                      <Input />
+                      <Button type="primary" className={styles.actionButton}>:)</Button>
+                  </div>
+              </Col>
+          </Row>
+          <Row type="flex" justify="center" align="center">
+
+              <Col span={8}>
+                  {todos.map(() => <ToDoItem />)}
+              </Col>
+          </Row>
+      </div>
   );
 }
 
+function ToDoItem() {
+    return (
+        <div className={styles.listItem}>
+            <Button className={styles.closeButton} type="primary" shape="circle" icon="close" size="small" />
+        </div>
+    )
+}
 export default App;
